1346730ddc35b5c682dc57a9a4f89c5b4ab25756
pavlymakram <pavlymakram2@gmail.com>
2024-10-17 02:02:46 +0300

    first commit

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-white bottom text-center mt-5">
+        <div class="container">
+            <p class="p-5 text-black">Powered BY : lumainary team
+                    <strong><a target="_blank" href="https://www.facebook.com/">our works</a></strong>
+            </p>
+        </div>
+    </div>
+</template>
+
+<style >
+a {
+    text-decoration: none;
+    color: #525252;
+}
+a:hover {
+    text-decoration: underline;
+}
+
+
+</style>
